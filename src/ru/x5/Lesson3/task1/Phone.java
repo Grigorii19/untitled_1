a51d615129f3e30f17eae362be0b14a7e547f902
GR <grisha.zaitcev@gmail.com>
2020-09-19 14:54:09 +0400

    Переделал 1 задание 3 урока. Разбил на Main и Phone.

--- src/ru/x5/Lesson3/task1/Phone.java
+++ src/ru/x5/Lesson3/task1/Phone.java
@@ -1,88 +1,39 @@
 package ru.x5.Lesson3.task1;
 
-class Main {
+import java.util.Arrays;
 
-    public static class Phone {
+public class Phone {
 
-        int number, weight;
-        String model;
-        String name = "Boris"; // Имя звонящего
+    private int number, weight;
+    private String model;
 
-        public void receiveCall() {     //Метод receiveCall с именем звонящего.
-
-            System.out.println("Звонит " + name);
-        }
-
-        public void receiveCall(String name, int number) {      //Перегруженный метод receiveCall с именей звонящего и номером звонящего.
-
-            System.out.println("\n" + "Звонит " + name + " номер: " + number);
-        }
-
-        public void getNumber() {       //Метод getNumber - возвращает номер телефона.
-
-            System.out.println("Номер: " + number);
-        }
-
-        public void sendMessage(int number1, int number2) {   //Метод sendMessage - вывод номеров телефонов, которым будет отправленно сообщение.
-
-            System.out.println("\n" + number1 + "  " + number2);
-        }
-
-        public Phone(int a, String b, int c) {      //Конструктор Phone. Принимает три параметра.
-
-            number = a;
-            model = b;
-            weight = c;
-
-            Phone ph = new Phone(1, "model");   //Вызов конструктора с двумя параметрами.
-        }
-
-        public Phone(int a, String b) {     //Конструктор Phone. Принимает два параметра.
-
-            number = a;
-            model = b;
-        }
-
-        public Phone() {    //Конструктор Phone без параметров.
-        }
+    public Phone(int number, String model, int weight) { //Конструктор с 3 параметрами. Вызван конструктор с 2мя параметрами.
+        this(number, model);
+        this.weight = weight;
     }
 
-    public static void main(String [ ] args) {
+    public Phone(int number, String model) { //Конструктор с 2 параметрами
+        this.number = number;
+        this.model = model;
+    }
 
-        Phone ph1 = new Phone();
-        Phone ph2 = new Phone();
-        Phone ph3 = new Phone();
-        Phone ph = new Phone();
+    public Phone() { //Пустой конструктор
+    }
 
-        ph1.number = 7999111;
-        ph1.model = "iPhone";
-        ph1.weight = 10;
+    public void receiveCall(String name) {  //Метод receiveCall с именем звонящего.
+        System.out.println("Звонит " + name);
+    }
 
-        ph2.number = 7999222;
-        ph2.model = "Nokia";
-        ph2.weight = 11;
+    public void receiveCall(String name, int number) {   //Перегруженный метод receiveCall.
+        System.out.println("Звонит " + name + " с номера " + number);
+    }
 
-        ph3.number = 7999333;
-        ph3.model = "Samsung";
-        ph3.weight = 12;
+    public int getNumber() { //Метод возвращает номер телефона
+        return number;
+    }
 
-        System.out.println("Number: " + ph1.number + ";" + " Model: " + ph1.model + ";" + " Weight: " + ph1.weight + ";");
-        System.out.println("Number: " + ph2.number + ";" + " Model: " + ph2.model + ";" + " Weight: " + ph2.weight + ";");
-        System.out.println("Number: " + ph3.number + ";" + " Model: " + ph3.model + ";" + " Weight: " + ph3.weight + ";");
+    public void sendMessage(int... number) { //Метод с аргументом переменной среды.
+        System.out.println(Arrays.toString(number));
 
-        System.out.println();
-        ph1.receiveCall();
-        ph1.getNumber();
-
-        System.out.println();
-        ph2.receiveCall();
-        ph2.getNumber();
-
-        System.out.println();
-        ph3.receiveCall();
-        ph3.getNumber();
-
-        ph.receiveCall(ph1.name, ph2.number);
-        ph.sendMessage(ph1.number, ph2.number);
     }
 }
